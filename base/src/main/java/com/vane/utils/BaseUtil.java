eccffc2615038f160ec3c3839583a9c0e38cd96f
ciminz <ciminz@163.com>
2018-05-31 17:53:45 +0800

    20180531 17:53update 修改获取序列号逻辑

--- base/src/main/java/com/vane/utils/BaseUtil.java
+++ base/src/main/java/com/vane/utils/BaseUtil.java
@@ -2,19 +2,30 @@ package com.vane.utils;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class BaseUtil {
 	public static String getSeqStr() {
-		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmssSSS");
-		int i=(int)(Math.random()*900)+100;
-		return sdf.format(date) + i;
+		int hashCodeV = UUID.randomUUID().toString().hashCode();
+		if(hashCodeV < 0) {
+			hashCodeV = -hashCodeV;
+		}
+		
+		String seq = String.format("%15d", hashCodeV).trim();
+		return seq;
 	}
 	
 	public static long getSeqLong() {
-		Date date = new Date();
-		SimpleDateFormat sdf = new SimpleDateFormat("yyMMddHHmmssSSS");
-		int i=(int)(Math.random()*900)+100;
-		return Long.valueOf(sdf.format(date) + i);
+		int hashCodeV = UUID.randomUUID().toString().hashCode();
+		if(hashCodeV < 0) {
+			hashCodeV = -hashCodeV;
+		}
+		
+		String seq = String.format("%15d", hashCodeV).trim();
+		return Long.valueOf(seq);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(getSeqLong());
 	}
 }
